dcab093628afb28ef16f7a1b4f7c5f8015e44307
gk <officialkepik@gmail.com>
2024-01-15 18:43:39 +0300

    Task has been updated.

--- src/Lesson15_01_2024/KnowledgeCheck.java
+++ src/Lesson15_01_2024/KnowledgeCheck.java
@@ -3,6 +3,11 @@ package Lesson15_01_2024;
 public class KnowledgeCheck {
     public static void main(String[] args) {
 
+        // Вариант №2
+        //  Вывести все двухзначные числа,
+        //  которые делятся на 2, но не делятся на 4,
+        //  но при этом делятся на 2 и на 3 одновременно
+
         for (int i = 10; i < 100; i++) {
             if (i % 2 == 0 && i % 3 == 0 && i % 4 != 0) {
                 System.out.print(i + " ");
