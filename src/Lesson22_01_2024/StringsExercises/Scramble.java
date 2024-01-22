a7ce932f78338a7b8012cfbebd95bd5cacec8c40
gk <officialkepik@gmail.com>
2024-01-22 21:28:33 +0300

    Updated

---
+++ src/Lesson22_01_2024/StringsExercises/Scramble.java
@@ -0,0 +1,16 @@
+package Lesson22_01_2024.StringsExercises;
+
+public class Scramble {
+    public static void main(String[] args) {
+        String first = "rkwqodl";
+        String second = "wworld";
+        StringBuilder sb = new StringBuilder(first);
+        boolean result = true;
+        for (int i = 0; i < second.length(); i++) {
+            int index = sb.toString().indexOf(second.charAt(i));
+            if (index < 0) result = false;
+            else sb.delete(index, index + 1);
+        }
+        System.out.println(result);
+    }
+} 
